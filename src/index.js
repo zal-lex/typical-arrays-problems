1c0f3b1a81b8ed1e5516df8a8da1d0f0b4be55cb
Alexey Zalyotov <alexey.zalyotov@rubyroidlabs.com>
2021-03-15 19:43:48 +0300

    fix: update to fix not array argument error

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return array.length > 0 ? array.sort((a, b) => a - b)[0] : 0;
+  return Array.isArray(array) && array.length ? array.sort((a, b) => a - b)[0] : 0;
 }
 
 exports.max = function max (array) {
-  return array.length > 0 ? array.sort((a, b) => b - a)[0] : 0;
+  return Array.isArray(array) && array.length ? array.sort((a, b) => b - a)[0] : 0;
 }
 
 exports.avg = function avg (array) {
-  return array.length > 0 ? array.reduce((a, b) => a + b, 0) / array.length : 0;
+  return Array.isArray(array) && array.length ? array.reduce((a, b) => a + b, 0) / array.length : 0;
 }
