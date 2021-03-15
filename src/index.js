d4b55e2b9ddbc1cde27f72e029fa70064e81ae8b
Alexey Zalyotov <alexey.zalyotov@rubyroidlabs.com>
2021-03-15 19:42:01 +0300

    fix: fix error when array has zero length

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return array.sort((a, b) => a - b)[0];
+  return array.length > 0 ? array.sort((a, b) => a - b)[0] : 0;
 }
 
 exports.max = function max (array) {
-  return array.sort((a, b) => b - a)[0];
+  return array.length > 0 ? array.sort((a, b) => b - a)[0] : 0;
 }
 
 exports.avg = function avg (array) {
-  return array.reduce((a, b) => a + b, 0) / array.length;
+  return array.length > 0 ? array.reduce((a, b) => a + b, 0) / array.length : 0;
 }
