07b2ef534346eb3a8acd52fbb1b4faab38c58e06
Alexey Zalyotov <alexey.zalyotov@rubyroidlabs.com>
2021-03-15 19:40:08 +0300

    feat: implement avg function and refactor the code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return array.sort((a, b) => {a - b})[0];
+  return array.sort((a, b) => a - b)[0];
 }
 
 exports.max = function max (array) {
-  return array.sort((a, b) => {b - a})[0];
+  return array.sort((a, b) => b - a)[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return array.reduce((a, b) => a + b, 0) / array.length;
 }
